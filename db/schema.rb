ffbff9e272786322a253f42f9640674c8ff1d5ef
Olivier38 <olivier.traveaux@gmail.com>
2016-04-10 08:35:46 +0200

    Add "Commencer" btn in nav

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409084440) do
+ActiveRecord::Schema.define(version: 20160409133445) do
 
   create_table "advices", force: :cascade do |t|
     t.string   "name"
